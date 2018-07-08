8eef90835a44c7a6338aadaca575671aff26eed3
Per Olav Flaten <perolavflaten@live.no>
2018-07-08 02:25:08 +0200

    Harvester from tutorial

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,21 @@
 import { ErrorMapper } from "utils/ErrorMapper";
+import { Harvester } from './roles/harvester';
 
 // When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
 // This utility uses source maps to get the line numbers and file names of the original, TS source code
 export const loop = ErrorMapper.wrapLoop(() => {
-  console.log(`Current game tick is ${Game.time}`);
+    console.log(`Current game tick is ${Game.time}`);
+    const harvester: Harvester = new Harvester();
 
-  // Automatically delete memory of missing creeps
-  for (const name in Memory.creeps) {
-    if (!(name in Game.creeps)) {
-      delete Memory.creeps[name];
+    for (const n in Game.creeps) {
+        const creep = Game.creeps[n];
+        harvester.run(creep);
     }
-  }
+
+    // Automatically delete memory of missing creeps
+    for (const name in Memory.creeps) {
+        if (!(name in Game.creeps)) {
+            delete Memory.creeps[name];
+        }
+    }
 });
